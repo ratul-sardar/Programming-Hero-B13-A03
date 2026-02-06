15a182b58ce925fcd301154b352d6ae44f5078e0
ratul-sardar <ratulsardar4747@gmail.com>
2026-02-06 22:41:32 +0600

    first problem solved

---
+++ b13-a3.js
@@ -0,0 +1,13 @@
+// function newPrice(currentPrice, discount) {
+//   let isPriceNumber = typeof currentPrice == "number" ? true : false;
+//   let isDiscountNumber = typeof discount == "number" ? true : false;
+
+//   if (isPriceNumber && isDiscountNumber && discount >= 0 && discount <= 100) {
+//     let totalDiscount = (currentPrice * discount) / 100;
+//     let discountPrice = currentPrice - totalDiscount;
+
+//     return discountPrice;
+//   } else {
+//     return "Invalid";
+//   }
+// }
